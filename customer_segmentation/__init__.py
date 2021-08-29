125747f207fd013409fb4f9abbd427cf21e88d66
anitabaral <baralanita2@gmail.com>
2021-08-29 07:45:08 +0545

    Adding basic componets of rfm analysis module

---
+++ customer_segmentation/__init__.py
@@ -0,0 +1,6 @@
+from .county_specific_data import CountryData
+from .customer_segments import CustomerDivisions
+from .load_data import LoadPreprocess
+from .rfm_score import RFMScore
+from .rfm_values import RFMData
+from .top_candidates import TopCandidates
