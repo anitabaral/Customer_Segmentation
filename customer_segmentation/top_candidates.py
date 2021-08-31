16c417624dc85e241fe1da2f73a1b6689e5853b2
anitabaral <baralanita2@gmail.com>
2021-08-31 10:50:17 +0545

    Adding function parametrs for getting the top candidates

--- customer_segmentation/top_candidates.py
+++ customer_segmentation/top_candidates.py
@@ -5,26 +5,26 @@ class TopCandidates:
     def __init__(self, path):
         self.rfm_df_score = RFMScore(path).combine_scores()
 
-    def ten_top_money_spenders(self):
+    def ten_top_money_spenders(self, top_k=10):
 
         self.rfm_df_score[self.rfm_df_score["rfm_score"] == "555"].sort_values(
             "monetary", ascending=False
-        ).head()
+        ).head(top_k)
 
         return self.rfm_df_score
 
-    def ten_top_stopping_possibility(self):
+    def ten_top_stopping_possibility(self, top_k=10):
 
         self.rfm_df_score[self.rfm_df_score["r_score"] <= 2].sort_values(
             "monetary", ascending=True
-        ).head(10)
+        ).head(top_k)
 
         return self.rfm_df_score
 
-    def ten_lost_customers(self):
+    def ten_lost_customers(self, top_k=10):
 
         self.rfm_df_score[self.rfm_df_score["rfm_score"] == "111"].sort_values(
             "recency", ascending=False
-        ).head(10)
+        ).head(top_k)
 
         return self.rfm_df_score
